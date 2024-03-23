0648dec1ff00e824a8651159f61b45acec3d4b22
João Paulo <jp.sport17@gmail.com>
2024-03-23 14:24:23 -0300

    First Commit

---
+++ src/main/java/com/api/medfacil/MedfacilApplication.java
@@ -0,0 +1,13 @@
+package com.api.medfacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedfacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedfacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/medfacil/MedfacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.medfacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedfacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
