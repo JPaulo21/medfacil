659e4ed69a88b98a064a282fbb06ea5e17496e96
João Paulo <jp.sport17@gmail.com>
2024-04-21 22:24:32 -0300

    feat: Adicionda exception de Response para usuário não encontrado

--- src/main/java/com/api/medfacil/web/exceptions/ApplicationControllerAdvice.java
+++ src/main/java/com/api/medfacil/web/exceptions/ApplicationControllerAdvice.java
@@ -2,13 +2,23 @@ package com.api.medfacil.web.exceptions;
 
 import com.api.medfacil.exceptions.CpfRegisteredException;
 import com.api.medfacil.exceptions.FullNumberRegisteredException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<ResponseError> usernameNotFoundException(UsernameNotFoundException e){
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ResponseError(e.getMessage()));
+    }
 
     @ExceptionHandler(CpfRegisteredException.class)
     public ResponseEntity<ResponseError> cpfRegisteredException(CpfRegisteredException e){
