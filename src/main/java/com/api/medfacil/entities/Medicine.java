abe411fe7388707e7fa0633f2b0528227f6e780c
João Paulo <jp.sport17@gmail.com>
2024-04-23 23:05:14 -0300

    feat: Criada entidade de Medicine

---
+++ src/main/java/com/api/medfacil/entities/Medicine.java
@@ -0,0 +1,32 @@
+package com.api.medfacil.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Medicine")
+@Table(name = "medicines")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Medicine {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String dosage;
+
+    @Column(nullable = false)
+    private String pharmaceuticalForm;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User userId;
+
+}
