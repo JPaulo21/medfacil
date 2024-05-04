3bbddb1b223d679f6c85e78fc4a3382c326ef953
João Paulo <jp.sport17@gmail.com>
2024-05-04 18:45:00 -0300

    feat: Implementado método para recuperar o usuário logado

--- src/main/java/com/api/medfacil/services/AuthService.java
+++ src/main/java/com/api/medfacil/services/AuthService.java
@@ -1,14 +1,28 @@
 package com.api.medfacil.services;
 
+import com.api.medfacil.entities.User;
+import com.api.medfacil.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
 
 @Service
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final UserService userService;
 
     public String generateRandomCode(){
         Random random = new Random();
         return String.valueOf(random.nextInt(9000) + 1000);
+    }
+
+    public User getUserAuthentication(){
+        SecurityContextHolder.getContext().getAuthentication().isAuthenticated();
+        String cpf = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
+        User user = userService.findByCpf(cpf);
+        return user;
     }
 }
